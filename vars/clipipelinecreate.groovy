1f80cd27a1e570fdf8c62a92e1e3369a49785bef
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-02-27 12:39:18 +0530

    Update clipipelinecreate.groovy

--- vars/clipipelinecreate.groovy
+++ vars/clipipelinecreate.groovy
@@ -11,6 +11,6 @@ env.name = projectName
 def call(){
   //sh 'az extension add --name azure-devops' 
   //sh "sudo cat /home/ec2-user/token.txt | az devops login --organization https://dev.azure.com/${organization}/"
-  sh "az pipelines create --name ${pipelinename} --repository repo2 --branch master --repository-type tfsgit --yml-path azure-pipelines.yml --org https://dev.azure.com/${organization} --project Demo1"
+  sh "az pipelines create --name ${pipelinename} --repository ${repoforpipeline} --branch ${branch} --repository-type tfsgit --yml-path azure-pipelines.yml --org https://dev.azure.com/${organization} --project ${projectname}"
 
 }
