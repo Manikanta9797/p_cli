315ffea8c5ca1fdf2882c0a75a70a603a65bee26
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-02-27 12:04:08 +0530

    Create cliwidetails.groovy

---
+++ vars/cliwidetails.groovy
@@ -0,0 +1,16 @@
+/*def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.environments.environment)
+String a=jsonObj.environments.environment.deploy.key
+//String a=jsonObj.alm.projects.project.name
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+env.name = projectName
+*/
+def call(){
+  //sh 'az extension add --name azure-devops' 
+  //sh "sudo cat /home/ec2-user/token.txt | az devops login --organization https://dev.azure.com/${organization}/"
+  sh "az boards work-item show --id 55 --org https://dev.azure.com/${organization}"
+
+}
