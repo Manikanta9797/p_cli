8c8a87cd0e1c67c6a8a3b06eba435eeb0b8c683a
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-02-27 12:12:16 +0530

    Update clilistpipelines.groovy

--- vars/clilistpipelines.groovy
+++ vars/clilistpipelines.groovy
@@ -1,3 +1,15 @@
-
+/*def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.environments.environment)
+String a=jsonObj.environments.environment.deploy.key
+//String a=jsonObj.alm.projects.project.name
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+env.name = projectName
+*/
+def call(){
+  //sh 'az extension add --name azure-devops' 
+  //sh "sudo cat /home/ec2-user/token.txt | az devops login --organization https://dev.azure.com/${organization}/"
   sh "az pipelines list --org https://dev.azure.com/${organization} -p ${projectname}"
 }
