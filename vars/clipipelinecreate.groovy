e58a8ddd801d6eb3971f1304cf8b84af8116e863
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-02-27 12:07:35 +0530

    Create clipipelinecreate.groovy

---
+++ vars/clipipelinecreate.groovy
@@ -0,0 +1,16 @@
+/*def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.environments.environment)
+String a=jsonObj.environments.environment.deploy.key
+//String a=jsonObj.alm.projects.project.name
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+env.name = projectName
+*/
+def call(){
+  //sh 'az extension add --name azure-devops' 
+  //sh "sudo cat /home/ec2-user/token.txt | az devops login --organization https://dev.azure.com/${organization}/"
+  sh "az pipelines create --name ${pipelinename} --repository repo2 --branch master --repository-type tfsgit --yml-path azure-pipelines.yml --org https://dev.azure.com/${organization} --project Demo1"
+
+}
