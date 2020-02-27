36fe98843b723f3f30a3c165f361f5f518debf9a
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-02-27 13:18:55 +0530

    Update clifetcharea.groovy

--- vars/clifetcharea.groovy
+++ vars/clifetcharea.groovy
@@ -1,3 +1,14 @@
-
+/*def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.environments.environment)
+String a=jsonObj.environments.environment.deploy.key
+//String a=jsonObj.alm.projects.project.name
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+env.name = projectName
+*/
+def call(){
+  //sh 'az extension add --name azure-devops' 
   sh "az boards area project list --org https://dev.azure.com/${organization} --project ${projectname}"
 }
