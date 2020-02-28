a78e573870d426a220a16fe8b943f54149e3af38
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-02-28 12:05:07 +0530

    Update cliwidetails.groovy

--- vars/cliwidetails.groovy
+++ vars/cliwidetails.groovy
@@ -11,6 +11,6 @@ env.name = projectName
 def call(){
   //sh 'az extension add --name azure-devops' 
   //sh "sudo cat /home/ec2-user/token.txt | az devops login --organization https://dev.azure.com/${organization}/"
-  sh "az boards work-item show --id 55 --org https://dev.azure.com/${organization}"
+  sh "az boards work-item show --id 55 --org https://dev.azure.com/${Organization}"
 
 }
