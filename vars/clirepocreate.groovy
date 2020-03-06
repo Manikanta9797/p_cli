b07fee1ccdbffa17e29a3c823388010729a048d4
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-03-06 16:08:53 +0530

    Update clirepocreate.groovy

--- vars/clirepocreate.groovy
+++ vars/clirepocreate.groovy
@@ -1,13 +1,15 @@
-/*def call(jsondata){
+def call(jsondata){
 def jsonString = jsondata
 //println(jsonString)
 def jsonObj = readJSON text: jsonString
 println(jsonObj.environments.environment)
-String a=jsonObj.environments.environment.deploy.key
+String Organization=jsonObj.environments.environment.deploy.Organization
+String projectname=jsonObj.environments.environment.deploy.projectname
+String New_Repository_Name=jsonObj.environments.environment.deploy.New_Repository_Name
 //String a=jsonObj.alm.projects.project.name
 String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
 env.name = projectName
-*/
+
 def call(){
   //sh 'az extension add --name azure-devops' 
   //sh 'sudo cat /home/ec2-user/token.txt | az devops login --organization https://dev.azure.com/vickysastryvs/'
