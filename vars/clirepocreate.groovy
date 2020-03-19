5d282d28bbac68ac5a8a803b7a205aa2d6e762e2
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-03-19 11:14:06 +0530

    Update clirepocreate.groovy

--- vars/clirepocreate.groovy
+++ vars/clirepocreate.groovy
@@ -9,6 +9,20 @@ String b = jsonObj.environments.environment.deploy.projectname
 String projectname = b.replaceAll("\\[", "").replaceAll("\\]","");
 String c = jsonObj.environments.environment.deploy.New_Repository_Name
 String New_Repository_Name = c.replaceAll("\\[", "").replaceAll("\\]","");
+
+String d = jsonObj.environments.environment.deploy.Title_of_workitem
+String Title_of_workitem = d.replaceAll("\\[", "").replaceAll("\\]","");
+String e = jsonObj.environments.environment.deploy.Type_of_workitem
+String Type_of_workitem = e.replaceAll("\\[", "").replaceAll("\\]","");
+String f = jsonObj.environments.environment.deploy.User_mail_for_workitem
+String User_mail_for_workitem = f.replaceAll("\\[", "").replaceAll("\\]","");
+String g = jsonObj.environments.environment.deploy.Pipeline_Name
+String Pipeline_Name = g.replaceAll("\\[", "").replaceAll("\\]","");
+String h = jsonObj.environments.environment.deploy.Source_code_repository
+String Source_code_repository = h.replaceAll("\\[", "").replaceAll("\\]","");
+String i = jsonObj.environments.environment.deploy.Branch
+String Branch = i.replaceAll("\\[", "").replaceAll("\\]","");
+
 //String a=jsonObj.alm.projects.project.name
 //String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
 //env.name = projectName
@@ -16,8 +30,23 @@ String New_Repository_Name = c.replaceAll("\\[", "").replaceAll("\\]","");
 
   //sh 'az extension add --name azure-devops' 
   //sh 'sudo cat /home/ec2-user/token.txt | az devops login --organization https://dev.azure.com/vickysastryvs/'
-  sh "sudo cat /home/ec2-user/token.txt | az devops login --organization https://dev.azure.com/${Organization}/"
-  sh "az repos create --name ${New_Repository_Name} --organization https://dev.azure.com/${Organization} --project ${projectname}"
+  sh "sudo cat /home/ec2-user/token.txt | az devops login --organization https://dev.azure.com/${Organization}/" //login
+  //create a repo
+  sh "az repos create --name ${New_Repository_Name} --organization https://dev.azure.com/${Organization} --project ${projectname}" 
+ 
+  
+  
+  // create workitem
+  sh "az boards work-item create --title ${Title_of_workitem} --type ${Type_of_workitem} --org https://dev.azure.com/${Organization}/ --project ${projectname} --assigned-to ${User_mail_for_workitem}"
+  // create pipeline
+  sh "az pipelines create --name ${Pipeline_Name} --repository ${Source_code_repository} --branch ${Branch} --repository-type tfsgit --yml-path azure-pipelines.yml --org https://dev.azure.com/${Organization} --project ${projectname}"
+  // fetch repos
+  sh "az repos list --org https://dev.azure.com/${Organization} -p ${projectname}"
+  //fetch iterations
+  sh "az boards iteration project list --org https://dev.azure.com/${Organization} --project ${projectname}"
+  // list pipelines
+  sh "az pipelines list --org https://dev.azure.com/${Organization} -p ${projectname}"  
+}
 
 }
 
