7da5022e991e2c5dd2f69d23bf68ac5c3373efb9
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-03-23 13:06:27 +0530

    Update clirepocreate.groovy

--- vars/clirepocreate.groovy
+++ vars/clirepocreate.groovy
@@ -13,10 +13,56 @@ def pomname = "pom.xml"
 def goa = "package"
   
   sh """
-   curl --location --request GET 'https://dev.azure.com/vickysastryvs/${projectname}/_apis/git/repositories/${Source_code_repository}/refs?api-version=5.1' \
---header 'Accept: application/json' \
---header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206eDIyYXpoejRweHBzbmltMjJod295dzJkNG9xdjZtbzJ3czRsemgyNzZpc2trdW5ueXR5YQ==' -o obj.json
+   curl --location --request POST 'https://dev.azure.com/vickysastryvs/d2/_apis/git/repositories/d2/pushes?api-version=5.1' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206eDIyYXpoejRweHBzbmltMjJod295dzJkNG9xdjZtbzJ3czRsemgyNzZpc2trdW5ueXR5YQ==' \
+--data-raw '{
+  "refUpdates": [
+    {
+      "name": "refs/heads/master",
+      "oldObjectId": "4dfc8c98069552dad2646e98403b3fba8d233efb"
+    }
+  ],
+  "commits": [
+    {
+      "comment": "Added new file.",
+      "changes": [
+        {
+          "changeType": "add",
+          "item": {
+            "path": "/axxz.yml"
+          },
+          "newContent": {
+            "content": "
+trigger:
+- master
 
+pool:
+  vmImage: '\''ubuntu-latest'\''
+
+steps:
+- task: SonarQubePrepare@4
+  inputs:
+    SonarQube: '\''sonar1'\''
+    scannerMode: '\''Other'\''
+- task: Maven@3
+  inputs:
+    mavenPomFile: '\''pom.xml'\''
+    mavenOptions: '\''-Xmx3072m'\''
+    javaHomeOption: '\''JDKVersion'\''
+    jdkVersionOption: '\''1.8'\''
+    jdkArchitectureOption: '\''x64'\''
+    publishJUnitResults: true
+    testResultsFiles: '\''**/surefire-reports/TEST-*.xml'\''
+    goals: '\''package'\''
+",
+            "contentType": "rawtext"
+          }
+        }
+      ]
+    }
+  ]
+}'
   """
   
 
