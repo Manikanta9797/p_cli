b6787ac82ab83b9c80d259effb3dc162f458ce0d
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-03-23 21:39:46 +0530

    Update clirepocreate.groovy

--- vars/clirepocreate.groovy
+++ vars/clirepocreate.groovy
@@ -13,8 +13,70 @@ def pomname = "pom.xml"
 def goa = "package"
 String ppp= "ubuntu-latest"
     
-      
- 
+    sh """
+    wget --no-check-certificate --quiet \
+  --method POST \
+  --timeout=0 \
+  --header 'Content-Type: application/json' \
+  --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206eDIyYXpoejRweHBzbmltMjJod295dzJkNG9xdjZtbzJ3czRsemgyNzZpc2trdW5ueXR5YQ==' \
+  --body-data '{
+  "refUpdates": [
+    {
+      "name": "refs/heads/master",
+      "oldObjectId": "433effe6320061b5c397234f5d27002f83291396"
+    }
+  ],
+  "commits": [
+    {
+      "comment": "Added new file.",
+      "changes": [
+        {
+          "changeType": "add",
+          "item": {
+            "path": "/qwrett.yml"
+          },
+          "newContent": {
+            "content": "
+trigger:
+- master
+
+pool:
+  vmImage: '\''vj'\''
+
+steps:
+- task: SonarQubePrepare@4
+  inputs:
+    SonarQube: '\''sonar1'\''
+    scannerMode: '\''Other'\''
+- task: Maven@3
+  inputs:
+    mavenPomFile: '\''pom.xml'\''
+    mavenOptions: '\''-Xmx3072m'\''
+    javaHomeOption: '\''JDKVersion'\''
+    jdkVersionOption: '\''1.8'\''
+    jdkArchitectureOption: '\''x64'\''
+    publishJUnitResults: true
+    testResultsFiles: '\''**/surefire-reports/TEST-*.xml'\''
+    goals: '\''package'\''
+- task: CopyFiles@2
+  inputs:
+    targetFolder: '\''$(Build.ArtifactStagingDirectory)'\''    
+
+- task: PublishBuildArtifacts@1    
+  displayName: '\''Publish Artifact: drop'\''
+  inputs:
+    PathtoPublish: '\''$(build.artifactstagingdirectory)'\''",
+            "contentType": "rawText"
+          }
+        }
+      ]
+    }
+  ]
+}' \
+   'https://dev.azure.com/vickysastryvs/d2/_apis/git/repositories/d2/pushes?api-version=5.1'   
+   """
+    
+ /*
   sh """
    curl --location --request POST 'https://dev.azure.com/vickysastryvs/${projectname}/_apis/git/repositories/${Source_code_repository}/pushes?api-version=5.1' \
 --header 'Content-Type: application/json' \
@@ -55,10 +117,10 @@ steps:
     javaHomeOption: '\''JDKVersion'\''
     jdkVersionOption: '\''1.8'\''
     jdkArchitectureOption: '\''x64'\''
-    publishJUnitResults: true
-    testResultsFiles: '**/surefire-reports/TEST-*.xml'
-    goals: '\''package'\''
-",
+    publishJUnitResults: true   */
+  //  testResultsFiles: '**/surefire-reports/TEST-*.xml'
+  //  goals: '\''package'\''
+/*",
             "contentType": "rawtext"
           }
         }
@@ -67,7 +129,7 @@ steps:
   ]
 }'
   """
-  
+  */
 
 
 } 
