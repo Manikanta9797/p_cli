ba4344ccfa66554b3e30e6e03d9202b7b0c97f72
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-03-23 11:41:10 +0530

    Update clirepocreate.groovy

--- vars/clirepocreate.groovy
+++ vars/clirepocreate.groovy
@@ -47,7 +47,7 @@ steps:
     scannerMode: 'Other'
 - task: Maven@3
   inputs:
-    mavenPomFile: '${pomname}'
+    mavenPomFile: '"${pomname}"'
     mavenOptions: '-Xmx3072m'
     javaHomeOption: 'JDKVersion'
     jdkVersionOption: '1.8'
