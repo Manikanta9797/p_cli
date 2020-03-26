722d1e979cfb53e5da60dc3a4ae9b24c8b1bb888
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-03-26 16:26:44 +0530

    Update clirepocreate.groovy

--- vars/clirepocreate.groovy
+++ vars/clirepocreate.groovy
@@ -56,7 +56,7 @@ steps:
     jdkVersionOption: '\''1.8'\''
     jdkArchitectureOption: '\''x64'\''
     publishJUnitResults: true   
-    testResultsFiles: '**/surefire-reports/TEST-*.xml'
+    #testResultsFiles: '**/surefire-reports/TEST-*.xml'
     goals: '\''package'\''
 ",
             "contentType": "rawtext"
